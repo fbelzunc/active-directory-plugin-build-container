1bbdd7fccff04a0ae9a05c01b794414bd180a310
Félix Belzunce Arcos <fbelzunc@gmail.com>
2017-11-03 11:15:35 +0100

    Change the storage driver for ubuntu

--- custom-dockerd-entrypoint.sh
+++ custom-dockerd-entrypoint.sh
@@ -5,4 +5,7 @@ $(which dind) dockerd ${DOCKER_OPTS} >/dev/stdout 2>&1 &
 sleep 1
 cd "/project"
 mvn  -Djenkins.test.timeout=1200 "$@"
+
+#mvn  -Djenkins.test.timeout=1200 -Dtest=hudson.plugins.active_directory.docker.TheFlintstonesTest#validateNoDomain test
+#mvn  -Djenkins.test.timeout=1200 -Dtest=hudson.plugins.active_directory.docker.TheFlintstonesTest#validateDomain test
 exit 0
