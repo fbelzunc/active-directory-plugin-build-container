eaf3ae2184482cbe132707e8e4a7c3d7cb552244
Félix Belzunce Arcos <fbelzunc@gmail.com>
2017-11-09 11:45:25 +0100

    Update

--- custom-dockerd-entrypoint.sh
+++ custom-dockerd-entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -ex -o pipefail
+#DOCKER_OPTS="--config-file=/etc/docker/daemon.json --storage-driver=${DOCKER_STORAGE_DRIVER}"
 DOCKER_OPTS="--config-file=/etc/docker/daemon.json"
 $(which dind) dockerd ${DOCKER_OPTS} >/dev/stdout 2>&1 &
 sleep 1
-cd "/project"
-mvn  -Djenkins.test.timeout=1200 "$@"
+
+mvn -B -Djenkins.test.timeout=1200 "$@"
 
 #mvn  -Djenkins.test.timeout=1200 -Dtest=hudson.plugins.active_directory.docker.TheFlintstonesTest#validateNoDomain test
 #mvn  -Djenkins.test.timeout=1200 -Dtest=hudson.plugins.active_directory.docker.TheFlintstonesTest#validateDomain test
