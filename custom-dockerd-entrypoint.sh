74a5b77f28618095092df37cd4706d41ccbac15a
Félix Belzunce Arcos <fbelzunc@gmail.com>
2017-11-17 11:33:22 +0100

    Increase the test timeout to 1200 segs

--- custom-dockerd-entrypoint.sh
+++ custom-dockerd-entrypoint.sh
@@ -3,5 +3,5 @@ set -ex -o pipefail
 DOCKER_OPTS="--config-file=/etc/docker/daemon.json"
 $(which dind) dockerd ${DOCKER_OPTS} >/dev/stdout 2>&1 &
 sleep 1
-mvn -B -Djenkins.test.timeout=600 "$@"
+mvn -B -Djenkins.test.timeout=1200 "$@"
 exit 0
